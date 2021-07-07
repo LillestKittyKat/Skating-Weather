f3132f86092457c09805153a389eda6052f8be41
alanvinton <bigeats1@googlemail.com>
2021-07-07 23:57:07 +0100

    commit on 'main'

--- app.js
+++ app.js
@@ -5,16 +5,23 @@ var button;
 
 console.log('starting');
 
-if (!navigator.geolocation) {
-    alert("navigator.geolocation is not available");
+function updateClicked() {
+    if (!navigator.geolocation) {
+        document.getElementById('alert').innerHTML = "navigator.geolocation is not available";
+    }
+    navigator.geolocation.getCurrentPosition(setPos);
 }
-navigator.geolocation.getCurrentPosition(setPos);
+
 
 
 function setPos(position) {
-    position[0] = position.coords.latitude;
-    position[1] = position.coords.longitude;
-
+    if (position) {
+        position[0] = position.coords.latitude;
+        position[1] = position.coords.longitude;
+    } else {
+        position[0] = 50;
+        position[1] = -1;
+    }
     document.getElementById('content2').innerHTML = "Lat: " + position[0].toFixed(2) + ", Lon: " + position[1].toFixed(2);
     // loadJSON('https://api.openweathermap.org/data/2.5/forecast?lat=' + position[0] + '&lon=' + position[1] + '&appid=24af55e05930810f0386c8f7559871e0&units=metric', gotData)
     // document.write(get_json(), fn)
@@ -27,6 +34,6 @@ function setPos(position) {
 function json_process(data) {
     console.log(data);
     // document.write(data.list[0]);
-    document.getElementById('content1').innerHTML = data.list[0].clouds.all;
+    document.getElementById('content1').innerHTML = data.city.name;
 
 }
