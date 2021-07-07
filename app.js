915129ff54af25e1ea9c7cc81c93a5a721d12243
alanvinton <bigeats1@googlemail.com>
2021-07-07 23:16:42 +0100

    commit on 'main'

--- app.js
+++ app.js
@@ -15,9 +15,9 @@ function setPos(position) {
     position[0] = position.coords.latitude;
     position[1] = position.coords.longitude;
 
-    document.write("Lat: " + position[0].toFixed(2) + ", Lon: " + position[1].toFixed(2))
-        // loadJSON('https://api.openweathermap.org/data/2.5/forecast?lat=' + position[0] + '&lon=' + position[1] + '&appid=24af55e05930810f0386c8f7559871e0&units=metric', gotData)
-        // document.write(get_json(), fn)
+    document.getElementById('content2').innerHTML = "Lat: " + position[0].toFixed(2) + ", Lon: " + position[1].toFixed(2);
+    // loadJSON('https://api.openweathermap.org/data/2.5/forecast?lat=' + position[0] + '&lon=' + position[1] + '&appid=24af55e05930810f0386c8f7559871e0&units=metric', gotData)
+    // document.write(get_json(), fn)
     fetch('https://api.openweathermap.org/data/2.5/forecast?lat=' + position[0] + '&lon=' + position[1] + '&appid=24af55e05930810f0386c8f7559871e0&units=metric')
         .then(response => response.json())
         .then(data => json_process(data));
@@ -25,8 +25,8 @@ function setPos(position) {
 }
 
 function json_process(data) {
-    console.log(data)
-        // document.write(data.list[0]);
-    json_content = document.getElementsByClassName('content')
-    json_content.innerHTML = "data"
+    console.log(data);
+    // document.write(data.list[0]);
+    document.getElementById('content1').innerHTML = data.list[0].clouds.all;
+
 }
